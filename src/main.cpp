c9aa2f87a281768f71329ff7cc668967c368bd46
Matteo Paonessa <matteo.paonessa@gmail.com>
2023-01-15 20:17:13 +0100

    Versioning

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char* argv[])
     QCoreApplication::setOrganizationName("SaeraSoft");
     QCoreApplication::setOrganizationDomain("saerasoft.com");
     QCoreApplication::setApplicationName("Caesium Image Compressor");
-    QCoreApplication::setApplicationVersion("2.3.0");
+    QCoreApplication::setApplicationVersion("2.3.0-beta");
 
 #ifdef NDEBUG
     qInstallMessageHandler(Logger::messageHandler);
