50acfcfb5677b65ec08340c9c0bb3bcf1d20315e
Victor Tang <vtange@users.noreply.github.com>
2024-10-25 18:26:35 -0700

    add missing line for CN, TW, JP

--- resources/i18n/caesium_zh_TW.ts
+++ resources/i18n/caesium_zh_TW.ts
@@ -590,7 +590,7 @@ Saved: %4 (%5%)</source>
     <message>
         <location filename="../../views/MainWindow.ui" line="450"/>
         <source>Progressive</source>
-        <translation type="unfinished"></translation>
+        <translation>累進</translation>
     </message>
     <message>
         <location filename="../../views/MainWindow.ui" line="1786"/>
