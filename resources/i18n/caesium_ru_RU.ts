d5d55a280f99493e149cab3800f33d39ade77131
Denisskas <110352433+Denisskas@users.noreply.github.com>
2024-12-30 17:52:33 +0400

    Update caesium_ru_RU.ts-2

--- resources/i18n/caesium_ru_RU.ts
+++ resources/i18n/caesium_ru_RU.ts
@@ -277,7 +277,7 @@ Saved: %4 (%5%)</source>
     <message>
         <location filename="../../src/MainWindow.cpp" line="1502"/>
         <source>Recompress failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Повторное сжатие не удалось</translation>
     </message>
     <message>
         <location filename="../../views/MainWindow.ui" line="23"/>
@@ -561,12 +561,12 @@ Saved: %4 (%5%)</source>
     <message>
         <location filename="../../views/MainWindow.ui" line="450"/>
         <source>Progressive</source>
-        <translation type="unfinished"></translation>
+        <translation>Прогрессивный</translation>
     </message>
     <message>
         <location filename="../../views/MainWindow.ui" line="776"/>
         <source>Deflate level</source>
-        <translation>Уровень сжатия Deflate</translation>
+        <translation>Уровень сжатия</translation>
     </message>
     <message>
         <location filename="../../views/MainWindow.ui" line="783"/>
@@ -754,17 +754,17 @@ Saved: %4 (%5%)</source>
     <message>
         <location filename="../../views/PreferencesDialog.ui" line="153"/>
         <source>Auto</source>
-        <translation type="unfinished">Авто</translation>
+        <translation>Авто</translation>
     </message>
     <message>
         <location filename="../../views/PreferencesDialog.ui" line="158"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Светлый</translation>
     </message>
     <message>
         <location filename="../../views/PreferencesDialog.ui" line="163"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Темный</translation>
     </message>
     <message>
         <location filename="../../views/PreferencesDialog.ui" line="350"/>
